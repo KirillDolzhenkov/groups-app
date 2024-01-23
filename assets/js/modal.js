0696d55596d185b1a10b89a9c526e4a7e17d7a09
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2024-01-23 14:29:46 +0300

    initial commit

---
+++ assets/js/modal.js
@@ -0,0 +1,90 @@
+/*const modalLinks = document.querySelectorAll(".modal-link");
+const body = document.querySelector("body");
+const lockPadding = document.querySelectorAll(".lock-padding");
+
+let unlock = true;
+const timeout = 800;
+
+if (modalLinks.length > 0) {
+    for (let i = 0; i < modalLinks.length; i++) {
+        const modalLink = modalLinks[i];
+        modalLink.addEventListener("click", function (e){
+            const modalName = modalLink.getAttribute("href").replace("#", "");
+            const currentModal = document.getElementById(modalName);
+            modalOpen(currentModal);
+            e.preventDefault();
+        });
+    }
+}
+
+const modalCloseIcon = document.querySelectorAll(".close-modal");
+if (modalCloseIcon.length > 0) {
+    for (let i = 0; i < modalLinks.length; i++) {
+        const el = modalCloseIcon[i];
+        el.addEventListener("click", function (e){
+            modalClose(el.closest(".modal"));
+            e.preventDefault();
+        });
+    }
+}
+
+function modalOpen(currentModal) {
+    if (currentModal && unlock) {
+        const modalActive = document.querySelector(".modal.open");
+        if (modalActive) {
+            modalClose(modalActive, false);
+        } else {
+            bodyLock();
+        }
+        currentModal.classList.add(".open");
+        currentModal.addEventListener("click", function (e) {
+            if (!e.currentTarget.closest(".modal__body")) {
+                modalClose(e.currentTarget.closest(".modal"));
+            }
+        });
+    }
+}
+
+function popupClose(modalActive, doUnlock = true) {
+    if (unlock) {
+        modalActive.classList.add(".open");
+        if (unlock) {
+            bodyUnLock();
+        }
+    }
+}
+
+function bodyLock() {}
+function bodyUnLock() {}*/
+
+
+const groupsBtn = document.querySelector(".group-btn");
+const modalEl = document.querySelector(".modal");
+
+groupsBtn.addEventListener("click", openModal);
+
+function openModal() {
+    modalEl.classList.add("modal--show");
+    document.body.classList.add("stop-scrolling");
+    /*modalEl.innerHTML = ``;*/
+
+    const btnClose = document.querySelector(".modal__close");
+    btnClose.addEventListener("click", () => closeModal());
+}
+
+function closeModal() {
+    modalEl.classList.remove("modal--show");
+    document.body.classList.remove("stop-scrolling");
+}
+
+window.addEventListener("click", (e) => {
+    if (e.target === modalEl) {
+        closeModal();
+    }
+});
+
+window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        closeModal();
+    }
+});
